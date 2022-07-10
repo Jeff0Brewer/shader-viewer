ebfdd3d774c642932c87b2cd31415b606d45222f
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-07-09 21:03:13 -0600

    change shader pattern

--- public/frag.glsl
+++ public/frag.glsl
@@ -4,11 +4,14 @@ uniform vec2 uDimensions;
 uniform float uTime;
 void main() {
     vec3 coord = vec3(gl_FragCoord.xy/uDimensions, uTime);
-
     vec3 col = vec3(0.0, 0.0, 0.0);
 
-    col.x = sin(coord.x*10.0 + uTime);
-    col.y = cos(coord.y*10.0 + uTime);
+    float time = uTime*.3;
+
+    coord.x += cos(coord.x + time);
+    coord.y += sin(coord.y + time);
+    col.x = sin(coord.x*100.0 + time);
+    col.z = cos(coord.y*100.0 + time);
 
 
     gl_FragColor = vec4(col, 1.0);
