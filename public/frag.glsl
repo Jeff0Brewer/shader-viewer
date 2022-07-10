3403e85995b0feab0cab9dcbedc0f15130597381
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-07-09 21:09:29 -0600

    improve pattern

--- public/frag.glsl
+++ public/frag.glsl
@@ -8,10 +8,10 @@ void main() {
 
     float time = uTime*.3;
 
-    coord.x += cos(coord.x + time);
-    coord.y += sin(coord.y + time);
-    col.x = sin(coord.x*100.0 + time);
-    col.z = cos(coord.y*100.0 + time);
+    coord.x += cos(coord.y + time);
+    coord.y += sin(coord.x + time);
+    col.x = sin(coord.x*60.0 + time);
+    col.z = cos(coord.y*60.0 + time);
 
 
     gl_FragColor = vec4(col, 1.0);
